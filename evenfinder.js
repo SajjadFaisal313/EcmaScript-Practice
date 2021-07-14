d51632aa6d89c9d13695697d2d638750b0ca62a7
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-14 12:09:53 +0530

    evenodd

---
+++ evenfinder.js
@@ -0,0 +1,10 @@
+function FindEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    }
+    else {
+        return false
+    }
+}
+
+console.log(FindEven(78787));
---
+++ evenfinderOpt.js
@@ -0,0 +1,3 @@
+const FindEven = (num) => (num % 2 === 0 ? "Even" : "Odd");
+
+console.log(FindEven(14654654));
