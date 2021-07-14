1fef3db0ced68e39dbdc9b0e1ed8c449de4601c1
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-14 12:28:13 +0530

    OddEvenDivisible

---
+++ divideby7.js
@@ -0,0 +1,3 @@
+const Divby7 = (num) => (num % 7 === 0 ? "Divisible" : "Indivisible")
+
+console.log(Divby7(105));
---
+++ findodd.js
@@ -0,0 +1,8 @@
+const FindOdd = (num) => (num % 2 === 1);
+
+console.log(FindOdd(468689))
+
+//  one more way to do 
+// const FindOdd = (num) => !(num % 2 === 0);
+
+// console.log(FindOdd(56456468))
