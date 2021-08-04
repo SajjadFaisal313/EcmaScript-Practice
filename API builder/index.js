7ec6ad6081cefda77d7f642da5229048b11fab83
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-04 17:08:26 +0530

    Update index.js

--- API builder/index.js
+++ API builder/index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("im sajjad faisal");
+    res.json({ data: ["arsalan", "aashir", "nameera"]});
+});
+
+app.get("/emp", (req, res) => {
+    res.send("im from udgir")
+});
+app.get("/dept", (req, res) => {
+    res.send("im react developer") 
 });
 
 if (app.listen(4000)) {
