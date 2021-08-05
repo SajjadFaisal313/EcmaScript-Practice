cbbf9bd368c36ea95c2624260ea397e1713fd295
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-05 12:53:42 +0530

    Update add.js

--- add.js
+++ add.js
@@ -2,6 +2,9 @@ function addMe(num1, num2) {
     var sum = num1 + num2;
     // console.log("sum of " + num1," and " + num2 + " is equal to " +sum);
     console.log(`sum of ${num1} and ${num2} is equal to ${sum}`);
+    // console.log(sum)
+    
+    
 }
 
 addMe(45, 44);
