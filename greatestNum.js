7c68205cbd9d342c77043637da11d877d1cc02eb
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-13 15:07:59 +0530

    Create greatestNum.js

---
+++ greatestNum.js
@@ -0,0 +1,11 @@
+const greatestNum = (arrNum) => {
+  let maxNum = 0;
+  arrNum.forEach((element) => {
+    if (element > maxNum) {
+      maxNum = element;
+    }
+  });
+  return maxNum;
+};
+
+console.log(greatestNum([2, 5, 9, 4, 15, 8, 19, 7]));
